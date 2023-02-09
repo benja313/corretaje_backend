08ffa7ade688ecfb2d1e476f4524111e4ae4d545
benjamin <benjamin.huete.n@gmail.com>
2023-02-09 11:42:51 -0300

    cambios cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,15 +15,16 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        /*header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application');
-        return $next($request);*/
-        return $next($request)
+        return $next($request);
+        
+        /*return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE,
 
         OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');*/
     }
 }
